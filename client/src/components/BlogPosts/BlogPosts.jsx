0a78169cc383d1fd83c59cf04a5e28d87b942ff2
ashley kildunne <ashleykildunne@ashleys-MBP.attlocal.net>
2020-09-21 17:39:03 -0700

    BlogPosts connected to App.js

---
+++ client/src/components/BlogPosts/BlogPosts.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './BlogPosts.css';
+// import BlogPost from '../BlogPost/BlogPost';
+
+const BlogPosts = () => {
+  return (
+    <div>
+      Hello World
+    </div>
+  )
+}
+
+export default BlogPosts; 
---
+++ client/src/screens/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import BlogPosts from '../../components/BlogPosts/BlogPosts';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <BlogPosts/>
+    </div>
+
+  )
+}
+
+export default Home;
