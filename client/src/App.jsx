c2eeddd8a4699e7700fb56e5ca396241a4108281
ahsikuddin1 <67656602+ahsikuddin1@users.noreply.github.com>
2020-09-23 12:22:37 -0400

    Edit Blogs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./screens/Home/Home";
 import BlogDetail from "./screens/BlogDetail/BlogDetail";
 import CreateBlog from "./screens/CreateBlog/CreateBlog";
+import EditBlog from "./screens/EditBlog/EditBlog"
 import { Route, Switch } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
         <Route exact path="/api/blogs" component={Home} />
         <Route exact path="/api/blogs/create" component={CreateBlog} />
         <Route exact path="/api/blogs/:id" component={BlogDetail} />
+        <Route exact path ="api/blogs/edit/:id" component={EditBlog} />
+       
       </Switch>
     </div>
   );
